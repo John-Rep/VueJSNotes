1d95ce9938ea335474709eefbfe1fc69dc1758c4
John-Rep <replogle.john@next-u.fr>
2025-01-20 14:48:41 +0100

    Exercice de ProductList

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import NextuViewHelloWorld from './views/NextuViewHelloWorld.vue';
+import ProductList from './views/ProductList.vue';
 </script>
 
 <template>
-  <NextuViewHelloWorld :count="3"/>
+  <table>
+      <tr>
+          <th>Product Id</th>
+          <th>Product Name</th>
+          <th>Product Description</th>
+          <th>Product Status</th>
+      </tr>
+    <ProductList :quantity="0" description="Des jolies fleurs" label="Fleurs"/>
+    <ProductList :id="2" :quantity="4" description="Des carottes" label="Carottes"/>
+    <ProductList :id="3" :quantity="7" description="Des concombres d'Espagne" label="Concombres"/>
+  </table>
 </template>
 
 <style></style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { computed, ref } from 'vue'
+
+export default{
+    name: 'ProductList',
+    props: {
+        id: {
+            type: Number,
+            default: 1,
+        },
+        label: {
+            type: String,
+            default: "No Label",
+        },
+        description: {
+            type: String,
+            default: "No Description",
+        },
+        quantity: {
+            type: Number,
+            default: 5,
+        },
+    },
+    setup(props) {
+        const id = props.id
+        const label = props.label
+        const desc = props.description
+        const status = computed(() => {
+            return props.quantity > 0 ? "Available" : "Unavailable"
+        })
+
+        return {
+            id,
+            label,
+            desc,
+            status,
+        }
+    },
+}
+</script>
+<template>
+    <tr>
+        <td>{{ id }}</td>
+        <td>{{ label }}</td>
+        <td>{{ desc }}</td>
+        <td>{{ status }}</td>
+    </tr>
+</template>
+<style></style>
